ec6f01a9589f2ace86f8eb1ab7b324329d98750e
Snakesystem <90812378+feri-irawansyah@users.noreply.github.com>
2025-09-13 20:03:30 +0700

    DOcs doang

--- graphql-on-actix-web/graphql-on-actix-web/migrations/20250619054512_create_users_table.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    full_name TEXT NOT NULL,
-    email TEXT NOT NULL
-);
--- graphql-on-actix-web/graphql-on-actix-web/src/connection/db.rs
+++
@@ -1,9 +0,0 @@
-use sqlx::PgPool;
-use dotenvy::dotenv;
-use std::env;
-
-pub async fn get_pg_pool() -> PgPool {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgPool::connect(&database_url).await.expect("Failed to create pool")
-}
